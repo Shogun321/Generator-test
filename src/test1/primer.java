20eeb78be60f42fad243eb1363d15b51d8bda06c
V.B <46461151+Shogun321@users.noreply.github.com>
2020-10-26 11:17:46 +0100

    2. Update

---
+++ src/test1/primer.java
@@ -0,0 +1,15 @@
+package test1;
+
+import org.jfugue.pattern.Pattern;
+import org.jfugue.player.Player;
+
+public class primer {
+    public primer() {
+        org.jfugue.pattern.Pattern pattern = new Pattern("T[Allegro]");
+        pattern.add("V0 E5s D#5s | E5s D#5s E5s B4s D5s C5s " + "V1 Ri | Riii ");
+
+
+        Player player = new Player();
+        player.play(pattern);
+    }
+}
